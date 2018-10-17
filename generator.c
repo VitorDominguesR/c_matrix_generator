1c9c8e37f4de78a58f5e4f723443341abbfd1702
VitorDominguesR <vitor_dmrb@hotmail.com>
2018-10-16 22:14:01 -0300

    check_matrix updated

--- generator.c
+++ generator.c
@@ -4,10 +4,10 @@
 
 
 void check_matrix(float matrix[20][20], int x, int y)
-{
-    if(matrix[x][y-1] < 20.0)
+{//checar o entorno do ponto
+    if(matrix[x][y-1] < 20.0 && matrix[x][y+1] < 20 && matrix[x-1][y] < 20 && matrix[x+1][y] < 20.0)
     {
-        printf("valor x: %d valor y:%d valor_matriz: %.1f", x, y, matrix[x][y-1]);
+        printf("valor x: %d valor y:%d valor_matriz: %.1f", x, y, matrix[x][y]);
     }
 }
 
@@ -17,6 +17,7 @@ int main() {
     
     float teste[20][20];
     
+    //gera matriz aleatória
     for( y = 0; y<postions;y++)
     {
         for(x=0;x < postions; x++)
@@ -29,10 +30,10 @@ int main() {
     {
         for(x=0;x < postions; x++)
         {
-            if(teste[x][y] < 20.0)
+            if(teste[x][y] < 20.0) //verifica se o numero é pequeno
             {
             //printf("%.1f ", teste[x][y]);
-                check_matrix(teste, x, y);
+                check_matrix(teste, x, y); //função para checar o entorno da matriz (essa função que devemos desenvolver)
                 printf("%c",'\n');
             }
         }
